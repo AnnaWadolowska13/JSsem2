2eca19f5df4e13db4736dd581bd3f39a4d27ffb8
Anna Wądołowska <73426222+AnnaWadolowska13@users.noreply.github.com>
2022-03-11 10:36:39 +0100

    Update task6.js

--- task6.js
+++ task6.js
@@ -24,10 +24,26 @@ class TranslationsEngine{
         letters.push("a", "y");
         return letters.join("");
     }
+    toEnglishFromPigLatin = (text) => {
+        let translatedText;
+        translatedText = text.split(" ").map(word => this.wordFromPigLatin(word));
+        translatedText[0] = translatedText[0].charAt(0).toUpperCase() + translatedText[0].slice(1); // upper case first letter of sentense
+        return translatedText.join(" ");
+    }
+    wordFromPigLatin = (word) => {
+        word = word.toLowerCase();
+        let letters = [...word]; // split to letters
+        letters.pop(); //remove y
+        letters.pop(); //remove a
+        letters.unshift(letters.pop());
+        return letters.join("");
+    }
 }
 
 let translationsEngine = new TranslationsEngine();
 let translatorToPigLatin = new Translator(translationsEngine.toPigLatin)
+let translatorFromPigLatin = new Translator(translationsEngine.toEnglishFromPigLatin)
 
 console.log(translatorToPigLatin.translate("The quick brown fox"))
 
+console.log(translatorFromPigLatin.translate("Hetay uickqay rownbay oxfay"))
